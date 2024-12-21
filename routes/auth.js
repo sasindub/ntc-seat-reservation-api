43cc254380a965aa1471cb09464d4e9b3896a84b
Sasindu Bandara <sasindu.bandara@innovative-e.com>
2024-12-22 00:17:08 +0530

    added .env file and connected mongo db

---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+const router = Router();
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *                 format: password
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
+router.post("/register", (req, res) => {
+  // Registration logic
+  res.status(201).send("User registered!");
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-import express from 'express';
+import express from "express";
+import { swaggerUi, swaggerDocs } from "./config/swagger.js";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-
+// Simple Home Route
 app.get("/", (req, res) => {
-    res.send("hellow Sever");
+  res.send("Hello, Server is running!");
 });
 
-app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000");
-})
+console.log(process.env.MONGO_URI);
+
+// Swagger Documentation Route
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Start Server
+const PORT = 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started at http://localhost:${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+});
