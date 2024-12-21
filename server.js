b469f367dd3d9a641ceb4b4b3ba22e9c2d528922
Sasindu Bandara <sasindu.bandara@innovative-e.com>
2024-12-21 20:59:55 +0530

    installed swagger

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+
+app.get("/", (req, res) => {
+    res.send("hellow Sever");
+});
+
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
 })
